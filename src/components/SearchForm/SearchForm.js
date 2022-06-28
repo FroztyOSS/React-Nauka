6f04287e67a5eeb9b5d8db5d7f6e6ecfd0f557da
Łukasz Mróz <lmroz95@gmail.com>
2022-06-28 20:48:25 +0200

    Add new Component

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,34 +1,37 @@
-import styles from './SearchForm.module.scss'
-import TextInput from '../TextInput/TextInput.js';
-import Button from '../Button/Button.js';
-import { useDispatch } from 'react-redux';
-import { useState } from 'react';
+import styles from './SearchForm.module.scss';
+import TextInput from '../TextInput/TextInput';
+import Button from '../Button/Button';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
 import { updateSearchString } from '../../redux/store';
 
 const SearchForm = () => {
 
-    const [searchString, setSearchString] = useState('');
+  const phrase = useSelector(state => state.searchString)
 
-    const dispatch = useDispatch();
-  
-    const handleSubmit = e => {
-      e.preventDefault();
-      dispatch(updateSearchString('searchString'));
-      setSearchString('');
-    };
+  const [searchString, setSearchString] = useState(phrase || '');
 
+  const dispatch = useDispatch();
 
-
-
-
-    return (
-        <form className={styles.searchForm} onSubmit={handleSubmit}>
-        <TextInput value={searchString} onChange={e => setSearchString(e.target.value)} placeholder="Search..." />
-            <Button>
-                <span className="fa fa-search" />
-            </Button>
-        </form>
-    );
+  const handleSubmit = e => {
+    e.preventDefault();
+    dispatch(updateSearchString(searchString));
   };
+
+  useEffect(() => {
+    return () => {
+    dispatch(updateSearchString(''));
+    }
+  }, []);
+
+  return (
+    <form className={styles.searchForm} onSubmit={handleSubmit}>
+      <TextInput value={searchString} onChange={e => setSearchString(e.target.value)} placeholder="Search..." />
+      <Button>
+        <span className="fa fa-search" />
+      </Button>
+    </form>
+  );
+};
 
 export default SearchForm;
--- src/redux/store.js
+++ src/redux/store.js
@@ -3,25 +3,40 @@ import initialState from './initialState';
 import shortid from 'shortid';
 import strContains from '../utils/strContains';
 
-
+//selectors
 export const getFilteredCards = ({ cards, searchString }, columnId) => cards
-.filter(card => card.columnId === columnId && strContains(card.title, searchString));
+  .filter(card => card.columnId === columnId && strContains(card.title, searchString));
 
+export const getAllColumns = state => state.columns;
 
-  export const getAllColumns = state => state.columns;
+export const getListById = ({ lists }, listId) => lists.find(list => list.id === listId);
 
-  export const addColumn = payload => ({ type: 'ADD_COLUMN', payload });
-  export const addCard = payload => ({ type: 'ADD_CARD', payload});
-  export const updateSearchString = payload => ({ type: 'UPDATE_SEARCHSTRING', payload})
+export const getColumnsByList = ({ columns }, listId) => columns
+  .filter(column => column.listId === listId);
+
+export const getAllLists = state => state.lists;
+
+export const getFavoriteCards = state => state.cards.filter(card => card.isFavorite === true);
+
+// action creators
+export const addColumn = payload => ({ type: 'ADD_COLUMN', payload });
+export const addCard = payload => ({ type: 'ADD_CARD', payload});
+export const updateSearchString = payload => ({ type: 'UPDATE_SEARCHSTRING', payload});
+export const addList = payload => ({ type: 'ADD_LIST', payload });
+export const toggleFavoriteCard = payload => ({ type: 'TOGGLE_CARD_FAVORITE', payload });
 
 const reducer = (state, action) => {
   switch(action.type) {
     case 'ADD_COLUMN':
       return { ...state, columns: [...state.columns, { ...action.payload, id: shortid() }]};
     case 'ADD_CARD':
-      return { ...state, cards: [...state.cards, { ...action.payload, id: shortid() }]};
+        return { ...state, cards: [...state.cards, { ...action.payload, id: shortid() }]};
     case 'UPDATE_SEARCHSTRING':
-      return { ...state, searchString: action.payload };
+        return { ...state, searchString: action.payload };
+    case 'ADD_LIST':
+        return { ...state, lists: [...state.lists, { ...action.payload, id: shortid() }]};
+    case 'TOGGLE_CARD_FAVORITE':
+        return { ...state, cards: state.cards.map(card => (card.id === action.payload) ? { ...card, isFavorite: !card.isFavorite } : card) };
     default:
       return state;
   }
